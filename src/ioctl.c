97f57c8a378295236d69f60b75cc48ce9e0b5d11
Derrik Pates <demon@now.ai>
2023-03-15 16:13:38 -0600

    [OPS-20653] Make this code work with PHP 7 and 8.

--- src/ioctl.c
+++ src/ioctl.c
@@ -1,13 +1,35 @@
 #include <php.h>
 #include <sys/ioctl.h>
 #include "ioctl.h"
+#define HAVE_SOCKETS 1
+#include <php_network.h>
+#include <ext/sockets/php_sockets.h>
+#include <stdio.h>
+#if PHP_VERSION_ID >= 70000 && PHP_VERSION_ID < 80000
+#include <Zend/zend_list.h>
+#endif
+
 
 // PHP ioctl extension
 // by dj.thd <dj.thd@hotmail.com>
 // UNSAFE! PHP developer is responsible of passing correct arguments to function
 
+/* {{{ argument information */
+#if PHP_VERSION_ID >= 80000
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_ioctl, 0, 3, MAY_BE_LONG | MAY_BE_FALSE)
+    ZEND_ARG_OBJ_INFO(0, res, Socket, 0)
+#elif PHP_VERSION_ID >= 70000 && PHP_VERSION_ID < 80000
+ZEND_BEGIN_ARG_INFO_EX(arginfo_ioctl, 0, 0, 4)
+    ZEND_ARG_INFO(0, socket)
+#endif
+    ZEND_ARG_TYPE_INFO(0, request, IS_LONG, 0)
+    ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, is_ptr, _IS_BOOL, 0)
+ZEND_END_ARG_INFO()
+/* }}} */
+
 zend_function_entry ioctl_functions[] = {
-            PHP_FE(ioctl, NULL)
+            PHP_FE(ioctl, arginfo_ioctl)
         PHP_FE_END
 };
 
@@ -60,25 +82,30 @@ PHP_FUNCTION(ioctl) {
 
 	zval *res;
 	zend_long request;
-	zend_string *data;
 	zend_bool is_ptr;
 
-	php_stream *stream;
+	php_socket *php_sock;
+	char *data;
+	size_t data_len;
 
-	int fd;
+#if PHP_VERSION_ID >= 80000
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "Olsb", &res, socket_ce, &request, &data, &data_len, &is_ptr) == FAILURE) {
+		RETURN_THROWS();
+	}
 
-	ZEND_PARSE_PARAMETERS_START(4, 4);
-		Z_PARAM_RESOURCE(res);
-		Z_PARAM_LONG(request);
-		Z_PARAM_STR(data);
-		Z_PARAM_BOOL(is_ptr);
-	ZEND_PARSE_PARAMETERS_END_EX(return);
+	php_sock = Z_SOCKET_P(res);
+	ENSURE_SOCKET_VALID(php_sock);
+#elif PHP_VERSION_ID >= 70000 && PHP_VERSION_ID < 80000
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "rlsb", &res, &request, &data, &data_len, &is_ptr) == FAILURE) {
+		RETURN_FALSE;
+	}
 
-	php_stream_from_zval(stream, res);
-	php_stdio_stream_data *stream_data = (php_stdio_stream_data*)stream->abstract;
-	fd = stream_data->fd;
+	if ((php_sock = (php_socket *)zend_fetch_resource(Z_RES_P(res), php_sockets_le_socket_name, php_sockets_le_socket())) == NULL) {
+		RETURN_FALSE;
+	}
+#endif
 
-	int result = ioctl(fd, (int)request, is_ptr ? ZSTR_VAL(data) : *(void**)(ZSTR_VAL(data)));
+	int result = ioctl(php_sock->bsd_socket, (int)request, is_ptr ? data : *(void**)(&data));
 	if(result == -1)
 	{
 		RETURN_FALSE;
